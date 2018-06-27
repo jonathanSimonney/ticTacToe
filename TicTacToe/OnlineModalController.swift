364f5fe381f4f0ef3ede06537a6601371ee295bb
SUP'Internet 08 <supinternet08@MacBook-Air-de-SUPInternet-41.local>
2018-06-27 17:08:31 +0200

    began work to make movement shown.

--- TicTacToe/OnlineModalController.swift
+++ TicTacToe/OnlineModalController.swift
@@ -21,9 +21,11 @@ class OnlineModalController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("online modal loaded")
         setButtonsBorder()
         setViewContent()
+        TTTSocket.sharedInstance.socket.on("movement") { (params, _) in
+            self.updateGrid(params: params)
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -31,13 +33,20 @@ class OnlineModalController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func CloseAction(_ sender: UIButton) {
-        self.dismiss(animated: false, completion: nil);
+    @IBAction func closeAction(_ sender: Any) {
+        self.dismiss(animated: false, completion: nil)
     }
     
     @IBAction func buttonTouched(_ sender: UIButton) {
         let boxPlayedNumber = sender.tag - 1
         TTTSocket.sharedInstance.socket.emit("movement", boxPlayedNumber)
+    }
+    
+    public func setAttrs(params: Any?){
+        let unwrappedDictJson = self.unwrappReturn(params: params)
+        self.xPlayerName = unwrappedDictJson["playerX"] as! String?
+        self.oPlayerName = unwrappedDictJson["playerO"] as! String?
+        self.isXPlayerTurn = unwrappedDictJson["currentTurn"] as! String == "x"
     }
     
     private func setButtonsBorder(){
@@ -55,12 +64,46 @@ class OnlineModalController: UIViewController {
         self.currentTurn.text = "Current turn : " + currentPlayer
     }
     
-    public func setAttrs(params: Any?){
+    private func unwrappReturn(params: Any?) -> [String: Any]{
         let paramsArray = params as! NSArray
-        let unwrappedDictJson = paramsArray[0] as! [String: Any]
-        self.xPlayerName = unwrappedDictJson["playerX"] as! String?
-        self.oPlayerName = unwrappedDictJson["playerO"] as! String?
-        self.isXPlayerTurn = unwrappedDictJson["currentTurn"] as! String == "x"
+        return paramsArray[0] as! [String: Any]
     }
-
+    
+    private func updateGrid(params: Any?){
+        let unwrappedDictJson = self.unwrappReturn(params: params)
+        print(unwrappedDictJson)
+        if (unwrappedDictJson["error"] == nil){
+            print(unwrappedDictJson)
+            self.makeMovement(params: unwrappedDictJson)
+        }else{
+            self.showError(errorType: unwrappedDictJson["err"] as! String)
+        }
+        //errors possible : wrong_movement, game_finished, not_your_turn
+    }
+    
+    private func makeMovement(params: [String: Any]){
+        params["index"]
+        params["player_played"]
+        params["win"]
+        params["player_play"]
+    }
+    
+    private func showError(errorType: String){
+        var errorMessage: String
+        
+        switch errorType{
+        case "wrong_movement":
+            errorMessage = "You can't play if the box is already full!"
+        case "game_finished":
+            errorMessage = "The game is already finished, you can't keep playing!"
+        case "not_your_turn":
+            errorMessage = "It's not your turn!"
+        default:
+            errorMessage = "Unknown error, please report the problem."
+        }
+        
+        let alert = UIAlertController(title: "Error", message: errorMessage, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
